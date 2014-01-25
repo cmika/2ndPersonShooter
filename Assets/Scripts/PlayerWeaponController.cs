2890a7812d8ce9a646c732cc04e4cb7f2ad56fce
sigfig <0x516F16@Gmail.com>
2014-01-25 13:38:09 -0600

    implemented the game

---
+++ Assets/Scripts/PlayerWeaponController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeaponController : MonoBehaviour {
+
+	bool firing;
+
+	public GameObject prefabCamera;
+	public GameObject prefabProjectile;
+
+	public GameObject activeCamera;
+	public GameObject activeProjectile;
+
+
+
+	public void Start() {
+		//grab ref to existing camera on game start
+		activeCamera = GameObject.Find("PlayerCamera");
+	}
+
+	public void Update() {
+		if(Input.GetKeyDown(KeyCode.Mouse0)){
+			StartFiring();
+		}
+	}
+
+	public void StartFiring() {
+
+		Transform o = GameObject.Find("Torso").transform;
+
+		//instantiate projectile at head bone position
+		activeProjectile = (GameObject) Instantiate(prefabProjectile, o.position, o.rotation);
+
+		Vector3 v = o.TransformDirection(Vector3.left * 200);
+
+		activeProjectile.rigidbody.AddForce(v);
+	}
+
+	public void FinishFiring(Vector3 pos) {
+		GameObject.Destroy(activeCamera);
+
+		activeCamera = (GameObject) Instantiate(prefabCamera, pos, Quaternion.identity);
+		activeCamera.GetComponent<CameraFollowBehaviour>().target = transform;
+	}
+}
---
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileBehaviour : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag != "Environment")
+			return;
+
+		GameObject.Find("Player").GetComponent<PlayerWeaponController>().FinishFiring(transform.position);
+		Destroy(this.gameObject);
+	}
+}
