edc30344daf223e79b50a3a7dd26aa8506eec0ba
Chris Mika <chrismika93@gmail.com>
2014-01-26 01:06:52 -0600

    Testing Shaders

---
+++ Assets/Scripts/ImagePostprocessing.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImagePostprocessing : MonoBehaviour {
+
+	public Material mat;
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination) {
+
+		Graphics.Blit (source, destination, mat);
+	}
+
+}
