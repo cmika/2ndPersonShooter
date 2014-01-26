bdf2a58016b884aba865eaf5956f3de2dd4f1275
sigfig <0x516F16@Gmail.com>
2014-01-26 11:34:40 -0600

    we're looking for a puzzle platforming app with one unique twist that sets it apart from the rest

---
+++ Assets/Scripts/CreditsMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsMenu : MonoBehaviour {
+
+	public bool visible;
+	public GameObject mainMenu;
+
+	public Texture2D bg;
+	public GUIStyle backs;
+
+	void OnGUI() {
+		if(!visible) return;
+
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), bg);
+
+		if(GUI.Button(new Rect(Screen.width - 512, Screen.height - 256, 512, 256), "", backs)) {
+			mainMenu.GetComponent<MenuBehaviour>().visible = true;
+			visible = false;
+		}
+	}
+}
---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBehaviour : MonoBehaviour {
+
+	public bool visible;
+	public GameObject creditsMenu;
+
+
+	public GUIStyle plays;
+	public GUIStyle credits;
+	public GUIStyle quits;
+	public Texture2D bg;
+
+	void OnGUI() {
+
+		if(!visible) return;
+
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), bg);
+
+		if(GUI.Button(new Rect(Screen.width - 512, 0, 512, 256), "", plays)) {
+			Application.LoadLevel(1);
+		}
+
+		if(GUI.Button(new Rect(Screen.width - 512, 256, 512, 256), "", credits)) {
+			creditsMenu.GetComponent<CreditsMenu>().visible = true;
+			visible = false;
+		}
+
+		if(GUI.Button(new Rect(Screen.width - 512, 512, 512, 256), "", quits)) {
+			Application.Quit();
+		}
+	}
+}
